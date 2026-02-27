9f3ccf2e505cc541bd043423c24764c0e640808c
grehochka82 <lobodina0607@mail.ru>
2026-02-27 06:37:14 +0300

    Add README documentation

--- app/src/androidTest/java/com/lobodina/tip_time_theme/TipUITest.kt
+++ app/src/androidTest/java/com/lobodina/tip_time_theme/TipUITest.kt
@@ -8,26 +8,29 @@ import java.text.NumberFormat
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
 
 class TipUITest {
     @get: Rule
     val composeTestRule = createComposeRule()
 
     @Test
-    fun calculate_20_percent_tip(){
+    fun calculate_20_percent_tip() {
         composeTestRule.setContent {
-            Tip_Time_ThemeTheme{
-                Surface(modifier = Modofier. fillMaxSize())
-                TipTimeLayout()
+            Tip_Time_ThemeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    TipTimeLayout()
+                }
             }
+            composeTestRule.onNodeWithText("Bill Amount")
+                .performTextInput("10")
+            composeTestRule.onNodeWithText("Tip Percentage")
+                .performTextInput("20")
+            val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+            composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+                "No node with this text this text was found."
+            )
         }
-        composeTestRule.onNodeWithText("Bill Amount")
-            .performTextInput("10")
-        composeTestRule.onNodeWithText("Tip Percentage")
-            .performTextInput("20")
-        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
-        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
-            "No node with this text this text was found."
-        )
     }
 }
